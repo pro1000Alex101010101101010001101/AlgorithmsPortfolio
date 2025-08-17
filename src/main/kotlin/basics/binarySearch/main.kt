4b10fce1685474fceb6e4494241954e399f1739e
Alex <1965167lol@gmail.com>
2025-08-17 12:56:07 +0300

    feat(binarySearch): добавить реализацию бинарного поиска и README
    - Реализован алгоритм бинарного поиска (myBinarySearch) в Kotlin
    - Подключен BenchmarkRepository для замера времени выполнения
    - Добавлен README с описанием работы алгоритма и примеров использования

--- src/main/kotlin/basics/binarySearch/main.kt
+++ src/main/kotlin/basics/binarySearch/main.kt
@@ -1,5 +1,28 @@
 package basics.binarySearch
 
+import benchmarkRepository.BenchmarkRepository
+import utils.DataSorter
+import kotlin.random.Random
+
+private val dataSorter = DataSorter
+
 fun main() {
-    println("binarySearch example")
+    BenchmarkRepository.run("myBinarySearch", 10_000) { list ->
+        val sortedList = list.sorted()
+        val item = sortedList[Random.nextInt(sortedList.size)]
+        myBinarySearch(sortedList, item)
+    }
+}
+
+fun myBinarySearch(list: List<Int>, item: Int): Any? {
+    var low = 0
+    var high = list.size - 1
+    while (high >= 0) {
+        val mid = (low + high) / 2
+        val guess = list[mid]
+        if (guess == item) return mid
+        else if (guess > item) high = mid - 1
+        else low = mid + 1
+    }
+    return null
 }
